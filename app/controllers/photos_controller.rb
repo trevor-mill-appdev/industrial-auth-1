7f80dcb69bb71bc4fd270a04e41d359c73be96ca
trevor-mill <trevor.million@chicagobooth.edu>
2022-04-19 20:22:01 +0000

    Locked down photo controller with before_actions

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,6 +1,14 @@
 class PhotosController < ApplicationController
   before_action :set_photo, only: %i[ show edit update destroy ]
 
+  before_action :ensure_owner_is_current_user, only: [:edit, :update, :destroy]
+
+  def ensure_owner_is_current_user
+    if @photo.owner != current_user
+      redirect_back fallback_location: root_path, alert: "Nice try"
+    end
+  end
+
   # GET /photos or /photos.json
   def index
     @photos = Photo.all
@@ -17,8 +25,13 @@ class PhotosController < ApplicationController
 
   # GET /photos/1/edit
   def edit
+
+    if @photo.owner != current_user
+      redirect_back fallback_location: root_path, alert: "Nice try"
+    end
   end
 
+  
   # POST /photos or /photos.json
   def create
     @photo = Photo.new(photo_params)

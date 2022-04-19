f38ae3308d04aad308680049b66f7364abb8d498
trevor-mill <trevor.million@chicagobooth.edu>
2022-04-19 21:16:46 +0000

    Locked down feed and discover to current user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,14 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[ show liked feed followers following discover ]
 
+  before_action :ensure_user_is_current_user, only: [:feed, :discover]
+
+  def ensure_user_is_current_user
+    if @user != current_user
+      redirect_back fallback_location: root_path, alert: "Nice try"
+    end
+  end
+
   private
 
     def set_user
